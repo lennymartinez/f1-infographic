abbab2c6abb660f5c27e391f9a84a54b6f1c29f8
Lenny Martinez <lennymartinezd@gmail.com>
2019-10-12 20:42:36 -0400

    work on graph

--- code/chart.js
+++ code/chart.js
@@ -1,7 +1,22 @@
-d3.csv("/data/winners_annual.csv")
-    .then(function(data) {
-        console.log(data);
-    })
-    .catch(function(error) {
-        alert(error);
-    })
+var cleanWinners = function (d) {
+  return {
+    team: d.team,
+    year: +d.year,
+    driver: d.driver
+  };
+};
+
+var cleanTeamActivity = function (d) {
+  return {
+    team: d.team,
+    year: +d.year,
+    driver: d.driver
+  };
+};
+
+Promise.all([
+  d3.csv('data/winners_annual.csv', cleanWinners),
+  d3.csv('data/teams_active_years.csv', cleanTeamActivity)
+]).then(function (allData) {
+  console.log(allData);
+});
